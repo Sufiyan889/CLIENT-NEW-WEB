2d05e2fd39b39257a89c4c06fc60be6a09fdf128
Sufiyan <muhammadsufiyan292@gmail.com>
2025-11-06 17:30:55 +0500

    CLIENT

---
+++ contact.js
@@ -0,0 +1,41 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdBNcfPgGFbNsD-4OL4iHo9mBhO3opy98",
+    authDomain: "client-website-fdd4a.firebaseapp.com",
+    databaseURL: "https://client-website-fdd4a-default-rtdb.firebaseio.com/",
+    projectId: "client-website-fdd4a",
+    storageBucket: "client-website-fdd4a.appspot.com",
+    messagingSenderId: "974723244847",
+    appId: "1:974723244847:web:bee5ae7bfaf331921d77da"
+  };
+
+  var app = firebase.initializeApp(firebaseConfig);
+  var db = firebase.database();
+  
+  function submitData() {
+
+    var name = document.getElementById("namebox").value.trim();
+    var email = document.getElementById("emailbox").value.trim();
+    var message = document.getElementById("message").value.trim();
+
+    var userObj = {
+      User_Name: name,
+      User_Email: email,
+      User_Message: message,
+    };
+
+    db.ref("user").push(userObj)
+  }
+  const questions = document.querySelectorAll('.faq-question');
+  questions.forEach((question) => {
+    question.addEventListener('click', () => {
+      const parent = question.parentElement;
+      parent.classList.toggle('active');
+    });
+  });
+
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.querySelector(".nav-links");
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
